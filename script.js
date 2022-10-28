926e342146e7530bf496ffd438173e07cdd15dcb
SmitE <eriksmit2001@gmail.com>
2022-10-28 18:10:34 +0200

    main site design tweaking and button for gwent

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function openLegend() {
     if (closed)
         for (let i = 0; i < rows.length; i++) {
             let text = rows[i].children[1];
-            text.style.paddingLeft = '10px';
+            text.style.paddingLeft = '2vh';
             text.style.opacity = '1';
             text.style.fontSize = 'inherit';
             text.style.visibility = 'visible';
