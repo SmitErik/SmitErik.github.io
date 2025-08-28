a6bca2f83c7ea851c2be1cbaf66c3926cad4b1f5
SmitErik <eriksmit2001@gmail.com>
2025-08-28 19:19:35 +0200

    diretorch v1.1 version tracking

---
+++ diretorch/download_script.js
@@ -0,0 +1,11 @@
+const urlParams = new URLSearchParams(window.location.search);
+if (urlParams.get('download') === 'true') {
+    const direTorchSetup = "https://github.com/SmitErik/SmitErik.github.io/releases/download/DireTorch/DireTorchSetup.exe";
+
+    const link = document.createElement('a');
+    link.href = direTorchSetup;
+    link.download = direTorchSetup.split("/").pop();
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
