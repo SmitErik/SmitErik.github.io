049cdc7473154cc6d3bc384c142e2a9e56eae5fe
SmitErik <eriksmit2001@gmail.com>
2025-08-29 15:09:19 +0200

    diretorch v1.2 overlay over everything fix

--- diretorch/download_script.js
+++ diretorch/download_script.js
@@ -1,11 +1,4 @@
 const urlParams = new URLSearchParams(window.location.search);
-if (urlParams.get('download') === 'true') {
-    const direTorchSetup = "https://github.com/SmitErik/SmitErik.github.io/releases/download/diretorch_v1.1/DireTorchSetup.exe";
 
-    const link = document.createElement('a');
-    link.href = direTorchSetup;
-    link.download = direTorchSetup.split("/").pop();
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
+if (urlParams.get('download') === 'true')
+    document.getElementById('download-link').click();
